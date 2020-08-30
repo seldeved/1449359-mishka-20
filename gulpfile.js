9ffe6b12574654fbec4be509c3a0d53d801ccbc0
Egor Seldev <eseldev@yandex.ru>
2020-08-30 13:42:59 +0300

    Оптимизация картинок и шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ const createWebp = () => {
 exports.webp = createWebp;
 // Sprite
 const sprite = () => {
-  return gulp.src("source/img/**/icon-*.svg")
+  return gulp.src("source/img/sprite/*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
